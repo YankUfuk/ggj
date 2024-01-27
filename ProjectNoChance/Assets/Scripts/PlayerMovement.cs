a3193d6ba57d517d34343bc5d223d29bb914b43d
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2024-01-27 20:27:03 +0300

    updates

---
+++ ProjectNoChance/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float strength = 5f;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * strength;
+            }
+        }
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;
+    }
+    
+}
