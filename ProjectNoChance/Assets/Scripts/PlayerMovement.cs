737193959f49e8e984ebedca6d3475aa41ca9592
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2024-01-27 21:36:44 +0300

    flying animation + player controller + background

--- ProjectNoChance/Assets/Scripts/PlayerMovement.cs
+++ ProjectNoChance/Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector3 direction;
-    public float gravity = -9.8f;
-    public float strength = 5f;
+    public float upForce = 200f;
+    private Rigidbody2D rb2d;
+    public string sceneName;
 
-    private void Update()
+    void Start() 
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Update() 
     {
         if(Input.GetMouseButtonDown(0))
         {
-            direction = Vector3.up * strength;
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(0, upForce));
+            
         }
-
-        if(Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            if(touch.phase == TouchPhase.Began)
-            {
-                direction = Vector3.up * strength;
-            }
-        }
-        direction.y += gravity * Time.deltaTime;
-        transform.position += direction * Time.deltaTime;
     }
-    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
 }
